c34097ecbff3353ab05b92464502c6d2be520ff0
Sharona Young <msrona806@gmail.com>
2023-04-19 17:33:20 -0400

    added css loaders

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { submitBudget } from "./utils";
+import './style.css';
 
 const submitBudgetEl = document.querySelector('#submitBudget')
 
 submitBudgetEl.addEventListener('click', function(event){
   event.preventDefault();
   submitBudget();
-  submitBudgetEl.classList.add('hidden');
+  document.getElementById('budgetAmt').classList.add('hidden');
 });
 
 console.log("Hello from index.js");
