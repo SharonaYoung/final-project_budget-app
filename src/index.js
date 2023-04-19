60eb3e5737a9908bf7bcffb9354f9533ed1dbcbe
Sharona Young <msrona806@gmail.com>
2023-04-19 16:36:39 -0400

    added function for first submit button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+import { submitBudget } from "./utils";
+
+const submitBudgetEl = document.querySelector('#submitBudget')
+
+submitBudgetEl.addEventListener('click', function(event){
+  event.preventDefault();
+  submitBudget();
+  submitBudgetEl.classList.add('hidden');
+});
+
+console.log("Hello from index.js");
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+export function submitBudget() {
+  console.log("starting budget");
+}
