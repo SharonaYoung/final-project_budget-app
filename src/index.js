6b55c8d061ff8af0ca744d693b21f9e07a0c5fde
Sharona Young <msrona806@gmail.com>
2023-04-24 20:18:36 -0400

    updated logic for functions

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { submitBudget, newCategory, showBudget } from "./utils";
+import { submitBudget, chkCategory } from "./utils";
 import './style.css';
 
 const budgetAmtForm = document.querySelector('#budgetAmt')
 const CatForm = document.querySelector('#catForm');
 
 // submit the starting budget amount
-budgetAmtForm.addEventListener('submit', function(event){
-  event.preventDefault();
-  submitBudget();
-  showBudget();
-  console.log("budget amount");
-});
+budgetAmtForm.addEventListener('submit', submitBudget);
 
 // submit a category and amount
-CatForm.addEventListener('submit', function(event) {
-  event.preventDefault();
-  newCategory();
-})
+CatForm.addEventListener('submit', chkCategory);
 
 console.log("Hello from index.js");
--- src/utils.js
+++ src/utils.js
@@ -1,50 +1,59 @@
 // submit budget amount and switch input fields displayed
-export function submitBudget() {
+export function submitBudget(event) {
+  event.preventDefault();
   document.getElementById('amt').classList.add('hidden');
   document.getElementById('categories').classList.remove('hidden');
   document.getElementById('budgetDisplay').classList.remove('hidden');
+  showBudget();
 };
 
-let categories = [];
-let amounts = [];
-const catInputEl = document.getElementById('catInput');
-const amtInputEl = document.getElementById('amtInput');
-
 // display budget values to the DOM
-const budgetDisplayEl = document.querySelector('#budgetDisplay');
 const budgetInput = document.querySelector('#budget');
+const catInputEl = document.querySelector('#catInput');
+const amtInputEl = document.querySelector('#amtInput');
 let budgetStart;
 let budgetRemaining;
-export function showBudget() {
+
+function showBudget() {
   budgetStart = parseInt(budgetInput.value);
+  budgetRemaining = budgetStart;
   console.log(budgetStart);
 
   // display budget values to the DOM
   document.getElementById('total').innerHTML = budgetStart
-  document.getElementById('balance').innerHTML = budgetStart;
+  document.getElementById('balance').innerHTML = budgetRemaining;
 }
 
+let categories = [];
+let amounts = [];
+
 // get values from input boxes and add to an array
-export function newCategory() {
-  if(budgetStart <= 0){
+export function chkCategory(event) {
+  event.preventDefault();
+  if(budgetStart === 0){
     alert("Balance is 0!!");
     catInputEl.value = "";
     amtInputEl.value = "";
   }
-  if(amtInputEl.value > budgetRemaining){
+  else if(amtInputEl.value > budgetRemaining){
     alert("Not enough in budget, enter lower amount");
     amtInputEl.value = "";
-  }
+  } 
   else {
-    categories.push(catInputEl.value);
+    newCategory();
+  }
+  console.log(categories, amounts);
+}
+
+function newCategory() {
+  categories.push(catInputEl.value);
     catInputEl.value = "";
     amounts.push(parseInt(amtInputEl.value));
     budgetRemaining = budgetStart -= amtInputEl.value;
     amtInputEl.value = "";
     // update remaining balance amount
     document.getElementById('balance').innerHTML = budgetRemaining;
-    console.log('remainging budget: ' + budgetRemaining);
-  }
+    console.log('remaining budget: ' + budgetRemaining);
 }
 
 /*
